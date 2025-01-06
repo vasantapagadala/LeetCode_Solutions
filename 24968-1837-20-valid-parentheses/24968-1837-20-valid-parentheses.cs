caa19d554f1cc29993e26303b13f9b6390dc7f8d
Vasanta Pagadala <168307117+vasantapagadala@users.noreply.github.com>
2025-01-05 20:16:25 -0600

    Time: 2 ms, Memory: 40.7 MB - LeetHub

---
+++ 24968-1837-20-valid-parentheses/24968-1837-20-valid-parentheses.cs
@@ -0,0 +1,29 @@
+public class Solution {
+    public bool IsValid(string s) {
+        Stack<char> stack = new Stack<char>();
+
+        for (int i = 0; i < s.Length; i++)
+        {
+            char c = s[i];
+            if( c == '(' || c == '{' || c == '[')
+            {
+                stack.Push(c);
+            }
+            else if( c == ')' || c == '}' || c == ']')
+            {
+                if(stack.Count == 0)
+                {
+                    return false;
+                }
+                char top = stack.Pop();
+                if((c == ')' && top != '(') || (c == '}' && top != '{') || (c == ']' && top != '['))
+                {
+                    return false;
+                }
+            }
+        }
+        if(stack.Count != 0)
+            return false;
+        return true;
+    }
+}
